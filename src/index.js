e0f8facfe30e8baf651f3630a08675020a449dbc
Bala <72062773+s3805520@users.noreply.github.com>
2021-06-12 19:38:42 +1000

    Frontend

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles.css";
 import { BrowserRouter, Route } from "react-router-dom";
+import Amplify from "aws-amplify";
+
+Amplify.config({
+    API: {
+        endpoints: [
+            {
+                name: "backend",
+                endpoint: "http://3.90.105.161:5000/",
+            },
+        ],
+    },
+});
 
 ReactDOM.render(
     <BrowserRouter>
